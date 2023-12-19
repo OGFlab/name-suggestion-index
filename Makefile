6a867d465a8452bd04c69a6dba3723021e4c5727
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2023-12-19 21:20:50 +0800

    BREAKING CHANGE!: Delete OSM related data

--- Makefile
+++
@@ -1,7 +0,0 @@
-all:
-	echo "{" > name-suggestions.json
-	sh build.sh
-	echo "}" >> name-suggestions.json
-
-clean:
-	rm -f name-suggestions.json
--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SOURCES=$(find */ -type f -name '*.json' | sort)
-
-for f in $SOURCES
-do
-    echo "    \""${f%".json"}"\":" >> name-suggestions.json
-    cat $f >> name-suggestions.json
-    echo "," >> name-suggestions.json
-done
