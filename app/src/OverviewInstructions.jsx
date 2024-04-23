75ae4b1d5c4a255f1b19873d7c8ce21ffa1e6c04
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2024-04-23 09:52:06 +0800

    fix: change description and differ from osmlab's.

--- app/src/OverviewInstructions.jsx
+++ app/src/OverviewInstructions.jsx
@@ -4,41 +4,17 @@ import { AppContext } from './AppContext';
 
 export function OverviewInstructions() {
   const context = useContext(AppContext);
-  const params = context.params;
-  const t = params.t;
-
-  // setup defaults for this tree..
-  let itemType, wikidataTag;
-  if (t === 'brands') {
-    itemType = 'brand';
-    wikidataTag = 'brand:wikidata';
-  } else if (t === 'flags') {
-    itemType = 'flag';
-    wikidataTag = 'flag:wikidata';
-  } else if (t === 'operators') {
-    itemType = 'operator';
-    wikidataTag = 'operator:wikidata';
-  } else if (t === 'transit') {
-    itemType = 'network';
-    wikidataTag = 'network:wikidata';
-  }
+  // const params = context.params;
+  // const t = params.t;
 
   return (
-    <div className='instructions'><span className='hi'>👋</span>Hi! This project is called <a target='_blank' href='https://github.com/osmlab/name-suggestion-index/'>name-suggestion-index</a>.<br/>
+    <div className='instructions'><span className='hi'>👋</span>Hi! This project is called <strong>ogfnsi</strong> and is fork of famous <a target='_blank' href='https://github.com/osmlab/name-suggestion-index/'>name-suggestion-index</a>, which is important for many editor in OSM and be seen as a infrastruce.<br/>
       <br/>
-      We've collected a list of common {itemType} names from <a target='_blank' href='https://www.openstreetmap.org'>OpenStreetMap</a>,
-      and we're matching them all to their preferred tags, including a <code>'{wikidataTag}'</code> tag.<br/>
+      This site is using the same technology and apply it to OGF. This instance is running on <strong>GitHub Action</strong> and link to the domain <strong>ogfnsi.laoshubaby.moe</strong>. 
+      <br/>(Maybe OGFlab will have its own domain name in the future)
       <br/>
-      This tag is pretty special because we can use it to link features in OpenStreetMap to records
-      in <a target='_blank' href='https://www.wikidata.org'>Wikidata</a>, a free and open knowledge database.
       <br/>
-      You can help us by adding {t} to the index, matching {t} to Wikidata identifiers,
-      or <a target='_blank' href='wikidata-warnings.htm'>improving</a> the {t}' Wikidata pages.<br/>
-      <br/>
-      Below is a list of categories used by OpenStreetMap. Each category displays a count of {t} <strong>"(complete / total)"</strong>,
-      where "complete" means the {t} have been matched to a Wikidata identifier and a logo.<br/>
-      <br/>
-      See <a target='_blank' href='https://github.com/osmlab/name-suggestion-index/blob/main/CONTRIBUTING.md'>CONTRIBUTING.md</a> for more info.<br/>
+      For more technical details you can visit <a target='_blank' href='https://github.com/osmlab/name-suggestion-index/blob/main/CONTRIBUTING.md'>CONTRIBUTING.md (Upstream)</a> for more info, or contact @laoshubaby or @lemuria. <br/>
     </div>
   );
 };
